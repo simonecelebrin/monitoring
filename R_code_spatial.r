fd6b6f7c5d5c98fb081102e7c99882fd0bb8ba77
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-04-01 17:56:56 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -24,3 +24,28 @@ bubble(meuse, "zinc")
 bubble(meuse, "zinc", main="Zinc concentration")
 #exercise: bubble copper in red
 bubble(meuse, "zinc", main="Zinc concentration", col="red")
+#per inserire un dataset in R creiamo una cartella senza capital letter nel disco C del computer e poi inseriamo i file cvs nella cartella
+#setting the working directory for windows
+setwd("C:/lab/")
+#associamo un nome ad un dataset e poi diciamo che c'è una riga titolo
+covid <- read.table("covid_agg.csv", head=TRUE)
+#scrivere TRUE o T è uguale
+head(covid)
+#facciamo un plot di numeri di casi per paesi
+attach(covid) #così facciamo i plot usando i nomi delle colonne
+attach(covid)
+plot (country, cases)
+#se non si fa attach(covid) si deve scrivere
+plot(covid$country, covid$cases)
+#ma non vediamo tutti i casi nel grafico, allora possiamo scrivere etichette in orizzontali o verticali, così dovremmo vederli.
+plot(country, cases, las=0)
+#las=0 si mettono labels parallele
+plot(country, cases, las=1) #orizzontal labels
+plot(country, cases, las=2) #perpendicular labels
+plot(country, cases, las=3) #vertical labels
+plot(country, cases, las=2, cex.axis=0.5) #per cambiare dimensione scritte
+#vediamo ggplot ha 3 componenti: dati, estetica della mappa, funzione geometrica che vuoi usare per graficizzare dati
+#intal ggplot2 package
+install.packets("ggplot2")
+
+
