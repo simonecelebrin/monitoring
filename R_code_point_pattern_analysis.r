df873959865ee28f201b451a58245e31db49cbb1
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-04-03 18:17:21 +0200

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -5,11 +5,11 @@ install.packages("spatstat")
 library(spatstat)
 attach(covid)
 head(covid)
-covids<- ppp(lon,lat, c(-180,180), c(-90,90))
-#controlla bene cosa significa questo codice qui sopra!
+#convert the data to a point pattern object using the spatstat command ppp
+covids<- ppp(lon,lat, c(-180,180), c(-90,90)) #The general form is covids<- ppp(x.coordinates, y.coordinates, x.range, y.range)
 #senza fare attach la funzione diventa
 #covids<- ppp(covis$lon, covid$lat, c(-180,180), c(-90,90))
-# c sta per clasterizzare i dati tutti assieme. se ho due numeri descriventi una certa variabile.
+# c sta per clasterizzare i dati tutti assieme. è il range
 # ora facciamo una mappa di densità
 d<- density(covids)
 plot(d)
