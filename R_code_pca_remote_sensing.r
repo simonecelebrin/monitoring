f302b74744b8ec6484e90c82a24d492a34f649d3
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-05-07 19:16:10 +0200

    Update R_code_pca_remote_sensing.r

--- R_code_pca_remote_sensing.r
+++ R_code_pca_remote_sensing.r
@@ -50,7 +50,7 @@ plot(p224r63_2011$B1_sre, p224r63_2011$B3_sre)
 #riduciamo la risoluzione dell'immagine per evitare che diventi troppo pesante
 p224r63_2011_res <- aggregate(p224r63_2011, fact=10)
 
-ora la funzione PCA la attuiamo con questo comando della libreria RStoolbox
+#ora la funzione PCA la attuiamo con questo comando della libreria RStoolbox
 p224r63_2011_pca <- rasterPCA(p224r63_2011_res)
 p224r63_2011_pca # per vedere le caratteristiche del PCA
 
@@ -75,4 +75,31 @@ summary(p224r63_2011_pca$model) #fa un sommario del modello
 #così ti spiega tutto riassumento per ogni PC plottato
 #e vediamo che il PC1 rappresenta il 99,83 ecc della variabilità, quindi le bande erano molto correlate. 
 
-#ora vediamo quanto ogni banda era correlata con l'altra con la funzione pairs
+#ora vediamo quanto ogni banda era correlata con l'altra con la funzione pairs()
+pairs(p224r63_2011)
+#tutte le bande sono molto correlate, per quello nel PC1 sta il 99,83%
+
+#ora componiamo una immagine nuova con il PC1, PC2, PC3
+
+plotRGB(p224r63_2011_pca$map, r=1,g=2,b=3, stretch="Lin") #ricerchiamo i nomi delle componenti (PC! è la prima ecc...
+
+#facciamo lo stesso con foto 1988
+p224r63_1988_res <- aggregate(p224r63_1988, fact=10) #aggreghiamo
+p224r63_1988_pca <- rasterPCA(p224r63_1988_res) #analisi di tutte le bande con PCA
+plot(p224r63_1988_pca$map, col=cl)
+summary(p224r63_1988_pca$model)
+#PC1 =99.56%
+pairs(p224r63_1988)
+
+#ora facciamo la differenza di PCA dal 2011 al 1988
+difpca<-p224r63_2011_pca$map - p224r63_1988_pca$map  #differenza tra PC1 2011 e PC1 1988 e poi PC2 e poi avanti
+cldif<- colorRampPalette(c("blue","black","yellow"))(100)
+#plottiamo solo PC1 del difpca
+difpca #per vedere componenti
+plot(difpca$PC1, cl=cldif ) 
+#ci mostra la piu grande possibile variazione tra due immagini 2011 e 2988, dove c'è stata la più grande variazione
+#abbiamo riassunto in un layer le 8 bande del 2011
+#abbiamo riassunto in un layer le 8 bande del 1988
+#abbiamo fatto la differenza fra i due, vedendo le variazione temporale
+
+
