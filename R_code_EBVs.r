0c92462e80d01134b3685dee3cb4002ad0107af8
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-05-27 17:27:39 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -49,3 +49,53 @@ plotRGB(snt,4,3,2, stretch="lin", main="original image")
 plot(sd_snt, col=cl, main="diversity")
 #si vede che la variabilità aumenta nelle zone di ecotono, i bordi da un ecosistema all'altro
 
+#############################################
+#stesso algoritmo applicato ad un altra immagine
+#cladonia exemple
+#dimostriamo che questo tipo di algoritmo si può applicare a qualsiasi immagine, ache prese fotografiche di un lichene
+setwd("C:/lab/")
+library(raster)
+#it's an RGB immages so we need to use Brick
+clad<-brick("cladonia_stellaris_calaita.JPG")
+plotRGB(clad,1,2,3, stretch="lin") #bande sono già in ordine
+# create the window
+window <- matrix(1, nrow = 3, ncol = 3) #numeber 1 is a fixed abritaty value that do not impact on the calculation
+#la funzione che usiamo è focal
+#guardiamo la correlzione delle 3 bande
+pairs(clad)
+#sono molto correlate, e potremmo usarne solo una, ma creiamo comunque un PCA
+library(RStoolbox)
+cladpca<-rasterPCA(clad)
+cladpca
+#abbiamo il call
+#il model
+#map
+#facciamo il summary
+summary(cladpca$model)
+#98% 
+#plottiamo in RGB il pca
+plotRGB(cladpca$map, 1, 2, 3, stretch="lin")
+
+#facciamo la misura con la moving windows
+sd_clad<-focal(cladpca$map$PC1, w=window, fun=sd) #fun=sd=standard deviation
+#qualora fosse troppo pesante e ci mettesse troppo tempo
+#possiamo aggregare l'immgagine
+PC1_agg<-aggregate(cladpca$map$PC1, fact=10)
+sd_clad_agg<-focal(PC1_agg, w=window, fun=sd)
+#creiamo una palette di colori
+cl <- colorRampPalette(c('yellow','violet','black'))(100)
+par(mfrow=c(1,3))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plotRGB(clad,1,2,3, stretch="lin")
+plot(sd_clad, col=cl)
+plot(sd_clad_agg, col=cl)
+
+#nel primo grafico a sinistra si vede la dev.standard normale e a destra quella aggregata
+#ci dice quanto è complesso l'individuo in ogni sua parte. 
+#ovviamente aggregandolo abbiamo un mosaico diverso dall'originale
+
+
+
+
+
+
