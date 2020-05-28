34c54d4a6f83d35ca485163f31641dde0406f305
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-05-28 10:34:05 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -7,67 +7,69 @@ data(meuse)
 head(meuse) #it's a spatial dataset, has some coordinates!
 #let's explain to R that we are gonna use data that has coordinates:
 coordinates(meuse)= ~x+y
-#dopo l'uguale descriviamo intervallo coordinate: X e Y
-#stiamo spiegando a meuse to think spatially
-# ~ è un simbolo vecchio che usa "sp"
+#after = we describe the coordinates intervall: X e Y
+#we are explaining meuse to think spatially
+# ~ it's an ols symbol used by "sp"
 plot(meuse)
-#vediamo i punti nello spazio
+#let's see spatial points
 spplot(meuse, "zinc")
-#con questo comando invece usiamo il dataset in una variabile localizzata. 
-#vediamo varie classi e quindi diverse concentrazioni di zinco
+#this command let us see a single variable of the dataset. 
+#we see different classes and so different zinc concentration
 #exercise: plot the spatial amount of copper)
 spplot(meuse, "copper")
-spplot(meuse, "copper") #per vedere anche il titolo che vogliamo
+spplot(meuse, "copper") #just to se also the title we want
 spplot(meuse, "copper", main= "Copper concentration")
-# per cambiare simbolo e vedere non in classi ma in cerchi dimensionati alla concentrazione
+#how to change type of rappresentation? 
+#in this way we see not classes but circles sized to concentration
 bubble(meuse, "zinc")
 bubble(meuse, "zinc", main="Zinc concentration")
 #exercise: bubble copper in red
 bubble(meuse, "zinc", main="Zinc concentration", col="red")
-#per inserire un dataset in R creiamo una cartella senza capital letter nel disco C del computer e poi inseriamo i file cvs nella cartella
+#to insert on R a dataset, we need to create a directory (without capital letter) into C disk
+#then we insert all the files we want inside
 #setting the working directory for windows
 setwd("C:/lab/")
-#associamo un nome ad un dataset e poi diciamo che c'è una riga titolo
+#we associate a name with a dataset and then say that there is a title row
 covid <- read.table("covid_agg.csv", head=TRUE)
-#scrivere TRUE o T è uguale
+#to writing TRUE or T is the same
 head(covid)
-#facciamo un plot di numeri di casi per paesi
-attach(covid) #così facciamo i plot usando i nomi delle colonne
-attach(covid)
+#let's do a plot of case numbers in each country
+attach(covid) #so we plot using column names
 plot (country, cases)
-#se non si fa attach(covid) si deve scrivere
+#if we don't use attach(covid) we need to write:
 plot(covid$country, covid$cases)
-#ma non vediamo tutti i casi nel grafico, allora possiamo scrivere etichette in orizzontali o verticali, così dovremmo vederli.
+#but we don't see all the cases into the graph, so we can  we can write labels horizontally or vertically, so we should see them
 plot(country, cases, las=0)
-#las=0 si mettono labels parallele
+#las=0 put labels parallels
 plot(country, cases, las=1) #orizzontal labels
 plot(country, cases, las=2) #perpendicular labels
 plot(country, cases, las=3) #vertical labels
-plot(country, cases, las=2, cex.axis=0.5) #per cambiare dimensione scritte
-#vediamo ggplot ha 3 componenti: dati, estetica della mappa, funzione geometrica che vuoi usare per graficizzare dati
+plot(country, cases, las=2, cex.axis=0.5) #to change text dimension
+
+#now we see ggplot. It has 3 components: data, map estetic, geometric function we want touse to graph the data
 #intal ggplot2 package
 install.packets("ggplot2")
-#per caricare uno spazio di lavoro
+#to load a work space
 load("arealavoro_R_covid.RData")
-#per vedere se ha attuato il comando 
+#just to see if R has implemented the command 
 ls()
-#ls è un comando che fa vedere tutte le elaborazioni che avevamo fatto nel file
-#shows what data sets and functions a user has defined. When invoked with no argument inside a function, ls returns the names of the function's local variables
-
-#introduciamo ggplot
-#è il più fancy packages in R to do beautiful graphs
+#ls is a command that shows all the processing made into the file
+#it shows what data sets and functions a user has defined. 
+#When invoked with no argument inside a function, ls returns the names of the function's local variables
+#let's introduce ggplot
+#it is the most fancy packages in R to do beautiful graphs
 library(ggplot2)
-#c'è un database chiamato MPG di macchine
+#ggplot has a cars database called MPG
 data(mpg)
 head(mpg)
-#3 componenti chiave GGPLOT: data, aes(fz estetiche), geometry 
-ggplot #fz to plot the data
+#3 key components in GGPLOT: data, aes(aestetic function), geometry 
+ggplot #function to plot the data
 ggplot(mpg, aes(x=displ, y=hwy)) + geom_point() #geometry is separated 
- #cambiamo la geometria del grafico usando le linee
+#let's change geometry of the graph using lines
 ggplot(mpg, aes(x=displ, y=hwy)) + geom_line()
 ggplot(mpg, aes(x=displ, y=hwy)) + geom_polygon()
-#torniamo al covid
-head(covid) #poi mettiamo fz ggplot con anche specificazione che dimensione punti è uguale al numero di casi
+#back to covid
+head(covid) #we use ggplot specifying that point dimension = number of cases
 ggplot(covid,aes(x=lon, y=lat, size=cases)) + geom_point()
 
 
