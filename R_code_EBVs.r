41ea9c20c0bbb963f608fc065e64e57028995004
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-06-08 12:50:27 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -1,45 +1,45 @@
 #Essential Biodiversity variables
-#calcoliamo alcuni indici statistici per la biodiversità
+#let's calculate some biodiversity statistics indices
 setwd("C:/lab/")
 library(raster)
-#raster è un comando che importa solo una banda
-#usiamo comando brick così importiamo tutte le bande
-snt<-brick("snt_r10.tif") #snt sta per sentinel 
+#raster it's a command that import only one band
+#we use brick so we import all the bands
+snt<-brick("snt_r10.tif") #snt means sentinel
 snt
-#4 bande con 
+#it shows 4 bands  
 #B1=blu
 #B2=green
 #B3=red
 #B4=NIR
-plot(snt)
-#R3 G2 B1  
-plotRGB(snt,3,2,1, stretch="lin")
+plot(snt) #it shows 4 rasters
+#R3 G2 B1  NIR
+plotRGB(snt,3,2,1, stretch="lin") #we can decide 3 bands order and plot togheter
 plotRGB(snt,4,3,2, stretch="lin")
 
-#calcoliamo la dev.stand (variabilità)
-#può essere calcolata solo per un layer
-#lo facciamo con l'analisi multivariabile (che da più variabile ne crea solo una)
-#quindi da queste 4 bande ne creiamo solo una
-#da li calcoliamo la dev sta
-pairs(snt) #per vedere la correlazione tra le due
+#let's calculate standard deviation (variabiility)
+#it can be calculated for only one layer
+#so we can obtain only one layer with multivariate analisys and calculate st.dev on this layer 
+#so from these 4 band we create only one layer
+#and on this we calculate st.deviation
+pairs(snt) #to se the correlation between all bands
 
-#CREIAMO QUINDI PCA
+#SO LET'S CREATE THE PCA
 library(RStolbox)
 sntpca<-rasterPCA(snt)
 sntpca
-summary(sntpca$model) #vediamo nel $model che PC1 riassume il 70%
+summary(sntpca$model) #we see into $model that PC1 explain the 70% of all data
 plot(sntpca$map)
 
-#ora plottiamo in RGB giusto per vedere le 3 componenti in modo carino psichedelico
+#Now we plot on RGB just to see three components into a good way (psichedelic)
 plotRGB(sntpca$map, 1, 2, 3, stretch="lin")
 
-#ora calcoliamo dev stand del PC1 con finestra pixel 5x5
+#now we calculate PC1 stand. dev PC1 with a pixel window 5x5
 
- #spieghiamo al software che unsiamo una moving window quella che analizza 5x5 
-#creiamo allora la matrice 5x5 (i suoi valori non ci interessano)
-window <- matrix(1, nrow = 5, ncol = 5) #tutte le finestre hanno valore 1 (così non impattano e sono considerate vuote)
-#la funzione per spostare la finestra è "focal"
-#focal fz significa che: it calculate values for the neighborhood of focal cells
+#let's say to R  that we use a moving window (that analize 5x5 pixels) 
+#we create a matrix 5x5 (we don't care about its values)
+window <- matrix(1, nrow = 5, ncol = 5) #all pixels have value 1(so they do not impact and are considered empty)
+#the function to move the window is "focal"
+#focal function means: it calculate values for the neighborhood of focal cells
 #quindi per un intorno def dalla windows lui calcola la fz che gli dici. e lo fa per tutti gli intorni possibili
 sd_str<- focal(sntpca$map$PC1, w=window, fun=sd) #sd=standard dev e w è la finestra creata prima
 cl <- colorRampPalette(c('dark blue','green','orange','red'))(100)
