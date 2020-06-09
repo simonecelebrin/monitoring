4b57588cc8e2fb00e89ddb3c74f2db279fbe3a4a
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-06-09 10:11:50 +0200

    Update R_code_ecosystem_functions.r

--- R_code_ecosystem_functions.r
+++ R_code_ecosystem_functions.r
@@ -27,42 +27,44 @@ copNDV100<- aggregate(copNDV10, fact=10)
 
 #NEW THEME
 
-#Ora guardiamo uno scenario per rendere la vita sostenibile
+#So now we want to study a scenario usefull to reach a sustainble life
+#we want to see the BIOMASS DIFFERENCE of amazon forest during one period.
 
 setwd("C:/lab/")
 library(raster)
-defor1<-brick("defor1_.jpg") #per importare le immagini con tute le bande
+defor1<-brick("defor1_.jpg") #to import the images with all the bands
 defor2<-brick("defor2_.jpg")
 
 # band 1: NIR
 # band2: R
 # band3: G
 
-plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
-plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin") #just to see
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin") #just to see
 
 par(mfrow=c(1,2))
 plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
 
-#ora facciamo i conti dell'NDVI
-dvi1<- defor1$defor1_.1 - defor1$defor1_.2 #il dollaro è usarto per parti diverse di R
+#now we calculate the NDVI index
+dvi1<- defor1$defor1_.1 - defor1$defor1_.2 #the dollar is used to link a band inside the image
 dvi2<- defor2$defor2_.1 - defor2$defor2_.2
 cl<- colorRampPalette(c("darkblue","yellow","red","black"))(100)
+#so we obtain 2 NDVI in 2 different period
 
 par(mfrow=c(1,2))
 plot(dvi1, col=cl)
 plot(dvi2, col=cl)
 
-difdiv=dvi1-dvi2 #ora facciamo la differenza fra i dvi
+difdiv=dvi1-dvi2 #now we do the difference between the two NDVI
 #warning message:
 #In dvi1 - dvi2 :
 #Raster objects have different extents. Result for their intersection is returned
-#ma non è un problema
+#but is not a problem
 
 dev.off()
 cld<- colorRampPalette(c("blue","white","red"))(100)
-plot(difdiv, col=cld)
+plot(difdiv, col=cld) #we plot the difference
 
 #histogram che mostra bil plot del dvi
 
