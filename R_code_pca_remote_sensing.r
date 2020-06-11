1c0cca37b8dcb4f714e27ff588bf545a27617251
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-06-11 17:27:20 +0200

    Update R_code_pca_remote_sensing.r

--- R_code_pca_remote_sensing.r
+++ R_code_pca_remote_sensing.r
@@ -1,7 +1,6 @@
-#vedremo la correlazione di varie bande in un grafico pca con più di 3 dimensioni
-#PCA=  ridurre il numero più o meno elevato di variabili che descrivono
- # un insieme di dati a un numero minore di variabili latenti,
- # limitando il più possibile la perdita di informazioni
+#let's see the CORRELATION OF DIFFERENT BANDS INTO A PCA GRAPH with more of 3 dimensions
+#PCA= it reduces the number of variables (that describes the data) to a lower latents variables number
+# it limits as much as possible the loss of informations
 
 
 setwd("C:/lab/")
@@ -16,16 +15,16 @@ p224r63_2011<-brick("p224r63_2011_masked.grd")
 #b4 NIR
 #b5 SWIR (medium infrared)
 #b6 thermal infrared
-#b7SWIR (un altro medium infrared, con un'altra lunghezza d'onda)
+#b7SWIR (other infrered, with a different wavelength)
 #b8 panchromatic
 
 plotRGB(p224r63_2011,r=5, g=4, b=3, stretch="Lin") 
 library(ggplot2)
 
-#facciamo lo stesso con la funzione ggplot
+#we do the same with ggpolot
 ggRGB(p224r63_2011, 5,4,3)
 
-#lo stesso con immagine 1988
+#the same with the 1988 image
 p224r63_1988<-brick("p224r63_1988_masked.grd")
 plotRGB(p224r63_1988,r=5, g=4, b=3, stretch="Lin")
 
@@ -33,73 +32,77 @@ par(mfrow=c(1,2))
 plotRGB(p224r63_2011,r=5, g=4, b=3, stretch="Lin") 
 plotRGB(p224r63_1988,r=5, g=4, b=3, stretch="Lin")
 
-#ora usiamo il metodo PCA
-#per vedere anche la percentuale nascosta basta cambiare gli assi
+#NOW LET'S USE THE PCA!!!
+#(to see the hidden percentage it's enought to change the aixs)
 dev.off()
 
-#ci aspettiamo che blu e rosso siano molto correlate perchè sono assorbite per la fotosintesi
-names(p224r63_2011) #per vedere i nomi delle bande
+#we attend to see that blu and red are so related  because they're absorbed for photosintesis
+names(p224r63_2011) #to see band's names
 # "B1_sre" "B2_sre" "B3_sre" "B4_sre" "B5_sre" "B6_bt"  "B7_sre"
 
 # "$" per unire 
 plot(p224r63_2011$B1_sre, p224r63_2011$B3_sre)
-#con questa funzione vediamo la correlazione b1 e b3, ed è positiva
+#with this function, as usual, we se the correlation between only two bands, (b1,b3) and it'hight and +
 
-#facciamo analisi PCA
+#let's DO THE PCA
 
-#riduciamo la risoluzione dell'immagine per evitare che diventi troppo pesante
+#let's reduce images dimension to avoid that they're too heavy
 p224r63_2011_res <- aggregate(p224r63_2011, fact=10)
 
-#ora la funzione PCA la attuiamo con questo comando della libreria RStoolbox
+#now we create the PCA with this function of RStoolbox librart
 p224r63_2011_pca <- rasterPCA(p224r63_2011_res)
-p224r63_2011_pca # per vedere le caratteristiche del PCA
+p224r63_2011_pca # just to see PCA characteristics
 
-#call: è solo l'indicazione della PCA svolta sul file
-#model: stiamo usando la correlazione tra le variabile quindi la matrice di covarianza
-#map: info sulla mappa
+#call: it's only the indication of the PCA done over the file
+#model: we are using the variables correlation, so it's shows the covariance matrix
+#map: map info
 
-#ora la plottiamo
-#ha diversi output: la call, il model e la mappa
-#quindi plottiamo la mappa usando il $
+#now we plot
+#it has different output: the call, the model and the map
+#so we plot the map using: $
 
 plot(p224r63_2011_pca$map)
 cl<- colorRampPalette(c("dark grey","grey","light grey"))(100)
 plot(p224r63_2011_pca$map, col=cl)
 
-#ci da 7 PC 
-#varie componenti: PC1 ci da tutte le componenti, e via via PC2, PC3 ecc riducono le componenti
-# il componente principale ci da magari il 90% di info e via via di meno gli altri. Il PC7 ha magari solo del rumore inutile
+#we obtain 7 PC 
+#different components: PC1 give us the main part of the components, and gradually PC2, PC3 etc (decreasing the number of components)
+# the main PC shows the 90% of all the informations, and the others gradually less. The PC7 maybe it only has unnecessary noise
 
-#ora come vedere la variazione di ogni componente in un PC
-summary(p224r63_2011_pca$model) #fa un sommario del modello 
-#così ti spiega tutto riassumento per ogni PC plottato
-#e vediamo che il PC1 rappresenta il 99,83 ecc della variabilità, quindi le bande erano molto correlate. 
+#now how to see the variation of each PCA
+summary(p224r63_2011_pca$model) #it does a summary of the model 
+#so it explain all and sum (riassume) each PCA
+#we see that PC1 rapresent 99,83 etc of all variability
+#so the bands are very correlated. 
 
-#ora vediamo quanto ogni banda era correlata con l'altra con la funzione pairs()
+#now we see how much each band was correlated to the others, with the function pairs()
 pairs(p224r63_2011)
-#tutte le bande sono molto correlate, per quello nel PC1 sta il 99,83%
+#all the bands are very correlated eachother
+#this is the reason why PC1 shows 99,83%
 
-#ora componiamo una immagine nuova con il PC1, PC2, PC3
+#now we create a new image with PC1, PC2, PC3
 
-plotRGB(p224r63_2011_pca$map, r=1,g=2,b=3, stretch="Lin") #ricerchiamo i nomi delle componenti (PC! è la prima ecc...
+plotRGB(p224r63_2011_pca$map, r=1,g=2,b=3, stretch="Lin") #before we have found the names of the differents PCA (PC1 is the first and...)
 
-#facciamo lo stesso con foto 1988
-p224r63_1988_res <- aggregate(p224r63_1988, fact=10) #aggreghiamo
-p224r63_1988_pca <- rasterPCA(p224r63_1988_res) #analisi di tutte le bande con PCA
+#we do the same with the 1988 image
+p224r63_1988_res <- aggregate(p224r63_1988, fact=10) #aggregation
+p224r63_1988_pca <- rasterPCA(p224r63_1988_res) #analysis of every band thank to the PCA
 plot(p224r63_1988_pca$map, col=cl)
 summary(p224r63_1988_pca$model)
 #PC1 =99.56%
 pairs(p224r63_1988)
 
-#ora facciamo la differenza di PCA dal 2011 al 1988
-difpca<-p224r63_2011_pca$map - p224r63_1988_pca$map  #differenza tra PC1 2011 e PC1 1988 e poi PC2 e poi avanti
+#now we do the DIFFERENCE between PCA 2011 and 1988
+difpca<-p224r63_2011_pca$map - p224r63_1988_pca$map  #it does the difference between PC1 1988 with 2011, then the PC2 2011 with PC2 of 1988 etc
 cldif<- colorRampPalette(c("blue","black","yellow"))(100)
-#plottiamo solo PC1 del difpca
-difpca #per vedere componenti
+#let's plot only the difference of PC1
+difpca #to see the components
 plot(difpca$PC1, cl=cldif ) 
-#ci mostra la piu grande possibile variazione tra due immagini 2011 e 2988, dove c'è stata la più grande variazione
-#abbiamo riassunto in un layer le 8 bande del 2011
-#abbiamo riassunto in un layer le 8 bande del 1988
-#abbiamo fatto la differenza fra i due, vedendo le variazione temporale
+#it shows the biggest possible variation between two images of 2011 and 1988
+#we know where there's been the biggest variation!!
+
+#so we have summarize into a single layer the 8 bands of 2011
+#we have summarize into a single layer the 8 bands of 1988
+#and we calculated the difference, seeing the temporal variation
 
 
