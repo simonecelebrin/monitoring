fa7d02933990fa0be133712c451dff6d085aaa59
simonecelebrin <62661646+simonecelebrin@users.noreply.github.com>
2020-06-21 18:47:50 +0200

    Update R_code_exercise.r

--- R_code_exercise.r
+++ R_code_exercise.r
@@ -4,6 +4,10 @@ setwd("C:/lab/ese/")
 library(raster)
 library(ncdf4)
 library(RStoolbox)
+library(ggplot2)
+library(rgdal)
+library(rasterVis)
+library(sf) 
 
 # C02 file 
 rlist<-list.files(pattern="odiac")
@@ -36,12 +40,13 @@ listPC1s<-stack(union)
 writeRaster(listPC1s, "PC1s_all.tif")
 
 serieC02<-brick("PC1s_all.tif")
-png("serieC02")
-plot(serieCO2, main="CO2 serie")
+png("serieC02.png",  width = 4, height = 4, units = 'in', res = 1200)
+plot(serieC02,, main="CO2 serie", col.main="red",font.main=4)
 dev.off()
+
 ########################
 #PREVISIONE CO2
-
+#serieC02<-brick("PC1s_all.tif")
 source("predictionCo2.r")
 plot(predicted.co2)
 click(predicted.co2, n=Inf, id=FALSE, xy=FALSE, cell=FALSE, type="n", show=TRUE)
@@ -51,13 +56,15 @@ plot(x, col=cl)
 writeRaster(x, "previsione_co2.tif")
 x100<-x*100
 plot(x100, col=cl)
-png("co2_prevista.png")
-plot(x100, col=cl, main= "Previsione CO2)
+png("co2_prevista.png", width = 4, height = 4, units = 'in', res = 3000)
+plot(x100, col=cl, main= "Previsione CO2")
 dev.off()
 
-#coste
+png("co2_prevista_confini.png", width = 4, height = 4, units = 'in', res = 3000)
+plot(x100, col=cl, main= "Previsione CO2")
 coastlines<- readOGR("ne_10m_coastline.shp")
-
+plot(coastlines, add=T,lwd=0.1)
+dev.off()
 
 #DIFFERENZA CO2
 dif<- (serieC02$PC1s_all.17 - serieC02$PC1s_all.1)
@@ -65,14 +72,14 @@ dif
 plot(dif)
 click(dif, n=Inf, id=FALSE, xy=FALSE, cell=FALSE, type="n", show=TRUE)
 d <- reclassify(dif, cbind(0.0007800222,NA))
-png("co2_dif.png")
+png("co2_dif.png", width = 4, height = 4, units = 'in', res = 1200)
 plot(d, col=cl, main="Differenza CO2 2018-2002")
 dev.off()
 
 #correlazione tra previsione e differenza
 plot(x,d)
-png("co2_corr_prev_diff.png", main="Correlazione fra CO2 prevista e differenza annuale", ylab="previsione", xlab="differenza")
-plot(x,d)
+png("co2_corr_prev_diff.png") 
+plot(x,d, main="Correlazione fra CO2 prevista e differenza annuale", ylab="Immagine previsione", xlab="Immagine dif")
 dev.off()
 
 #TREND NE TEMPO 
@@ -83,37 +90,14 @@ dev.off()
 #x=y is the not changeing line 
 #so, for each pixel there's a place into the graph and the values of the two images (of thesame pixels) are related
 #if values are the same, that pixel will be on the straight line, otherwise no
-
-
-png("TrendC02.png")
-plot(serieC02$PC1s_all.1, serieC02$PC1s_all.17, main="CO2 variation (2018-2002)")
+png("Trend C02.png")
+plot(serieC02$PC1s_all.1, serieC02$PC1s_all.17, main="CO2 variation (2018-2002)", ylab="CO2 2018", xlab="CO2 2002")
 abline(0,1,col="red")
 dev.off()
-
-#create a box plot
-#y aix: the different times 
-#boxplot takes in any number of numeric vectors, drawing a boxplot for each vector
-boxplot(serieC02)
-#in black the outlayers
-#to remove them:
-boxplot(serieC02, outline=F) #F=false
-#to make it orizontaly
-boxplot(serieC02, outline=F, horizontal=T) #T=true
-#put the axes names
-boxplot(serieC02, outline=F, horizontal=T, axes=T)
-png("Boxplot Co2C02.png")
-boxplot(serieC02, outline=F, horizontal=T, main="Boxplot CO2") #T=true
-dev.off()
-#the bolt black line is the median
-#the lines at the ends are minimum and maximus
-#over time maximus is decreased 
 
 #############
 
 #NDVI
-setwd("C:/lab/ese/")
-library(raster)
-library(ncdf4)
 rNlist<-list.files(pattern="c_gls_NDV")
 rNlist
 importN<-lapply(rNlist,raster)
@@ -125,10 +109,24 @@ clN <- colorRampPalette(c('light green','green','dark green'))(100)
 plot(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.1)
 click(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.1, n=Inf, id=FALSE, xy=FALSE, cell=FALSE, type="n", show=TRUE)
 NDVI.multitempR<- calc(NDVI.multitemp, fun=function(x){ x[x > 0.936000] <- NA; return(x)} )
+
+#or: faster for PC:
+NDVI_corr.1<- calc(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.1, fun=function(x){ x[x > 0.936000] <- NA; return(x)} )
+NDVI_corr.2<- calc(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.2, fun=function(x){ x[x > 0.936000] <- NA; return(x)} )
+NDVI_corr.3<- calc(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.3, fun=function(x){ x[x > 0.936000] <- NA; return(x)} )
+NDVI_corr.4<- calc(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.4, fun=function(x){ x[x > 0.936000] <- NA; return(x)} )
+NDVI_corr.5<- calc(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.5, fun=function(x){ x[x > 0.936000] <- NA; return(x)} )
+NDVI_corr.6<- calc(NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.6, fun=function(x){ x[x > 0.936000] <- NA; return(x)} )
+NDVI.multitempR<-stack(NDVI_corr.1,NDVI_corr.2,NDVI_corr.3,NDVI_corr.4,NDVI_corr.5,NDVI_corr.6)
+png("NDVI serie.png",width = 4, height = 4, units = 'in', res = 3000) 
+plot(NDVI.multitempR, main="NDVI serie")
+dev.off()
+#############
+
 writeRaster(NDVI.multitempR, "NDVI_corr.tif")
 #NDVI 3 periods in rgb
-png("TrendC02.png")
-plotRGB(NDVI.multitempR, r=1, g=4, b=6, stretch="Lin", main="RGB NDVI 1998-2010-2020") #bello
+png("NDVI_RGB_serie 1998-2020.png",width = 6, height = 4, units = 'in', res = 1200)
+plotRGB(NDVI.multitempR, r=1, g=4, b=6, stretch="Lin",main="RGB NDVI 1998-2010-2020") #bello
 dev.off()
 #so where there are highter values the image thakes the red, green or blue color
 #so we understand in wich month there's been the highter values and where!!!
@@ -147,69 +145,43 @@ sd_str<- focal(aggr_NDVI, w=window, fun=sd)
 cl <- colorRampPalette(c('dark blue','green','orange','red'))(100)
 writeRaster(sd_str, "Dev.st_NDVI.tif")
 
-png("Dev.st_NDVI.png")
-par(mfrow=c(1,2))
+png("Dev.st_NDVI.png", width = 6, height = 8, units = 'in', res = 3000)
+par(mfrow=c(2,1))
 plot(sd_str, main="Dev.standard NDVI")
-plot(NDVI.multitempR$NDVI_rec.6, main="NDVI")
+plot(NDVI.multitempR$NDVI_corr.6, main="NDVI")
 dev.off()
-
-png("Dev.st_NDVI_re300.png", width = 465, height = 225, units='mm', res = 300)
-par(mfrow=c(1,2))
-plot(sd_str, main="Dev.standard NDVI",col.main="red")
-plot(NDVI.multitempR$NDVI_rec.6, main="NDVI")
-dev.off()
-
-ggsave("Dev.st_NDVI_res400.png", width = 3.25, height = 3.25, dpi = 600)
-
-plot(NDVI.multitempR$NDVI.multitemp$Normalized.Difference.Vegetation.Index.1KM.6,main="original image") 
-plot(sd_str, col=cl, main="diversity")
 #veriability increases on ecotone zones, or rather the borders between ecosystems
 ########################################
-#solo per ora
-sd_str<- focal(NDVI.multitempR$NDVI_rec.6, w=window, fun=sd)
-NDVI_rec.6
 
-LINEAR MODEL BETWEEN CO2 and NDVI
-
-library(rasterVis)
-library(sf) 
-NDVI2020<-raster("c_gls_NDVI_202006010000_GLOBE_PROBAV_V2.2.1.nc")
-Co22020<-raster("odiac2019_1x1d_2018.nc")
-#we define the random function
-random.points <- function(x,n) #n=number of random points
- #x=maximum number of pixel that the function can see. Because the two rasters have the same ammount of pixels
- #I can use one or the other, it doesn't matter
- #we take only 1000 points among all the pixels
-{
-lin <- rasterToContour(is.na(x))
-pol <- as(st_union(st_polygonize(st_as_sf(lin))), 'Spatial') # st_union to dissolve geometries
-pts <- spsample(pol[1,], n, type = 'random')
-}
-#now we do it
-pts<-random.points(NDVI2020,1000)
-
-#now we associate the NDVI2020 and Temp2020 values to these random points 
-#with the function EXTRACT
-pts <- random.points(NDVI2020,1000) #we use only
-NDVI2020p <- extract(NDVI2020, pts)
-CO22020p <- extract(CO22020,pts)
-
-
-model<-lm(NDVI2020p ~ Temp2020p)
-summary(model)
-plot(NDVI2020p,Temp2020p,col="green")
-abline(model1, col="red")
-
-#######################
 #general model:
 NDVI2020<-raster("c_gls_NDVI_202006010000_GLOBE_PROBAV_V2.2.1.nc")
-Co22020<-raster("odiac2019_1x1d_2018.nc")
-Temper2020<-raster("c_gls_LST10-TCI_202006010000_GLOBE_GEO_V1.2.1.nc")
+serieC02<-brick("PC1s_all.tif")
+CO2ult<-serieC02$PC1s_all.1
+Temper2020<-raster("c_gls_LST10-DC_202006110000_GLOBE_GEO_V1.2.1.nc")
+Costru2020<- raster("lulc-human-modification-terrestrial-systems_geographic.tif")
 
-model <- lm(sales ~ youtube + facebook + newspaper, data = marketing)
-summary(model)
+NDVI2020r <- resample(NDVI2020, CO2ult, resample='bilinear') 
+Temper2020r<-resample(Temper2020, CO2ult, resample='bilinear') 
+Costru2020r<-resample(Costru2020, CO2ult, resample='bilinear')
+modelvariables<-stack(NDVI2020r,Temper2020r,CO2ult,Costru2020r)
+writeRaster(modelvariables, "stack_variables.tif")
 
 
+vuln<-rasterPCA(modelvariables)
+vulnPC1_stand<-(vuln$map$PC1)/maxValue(vuln$map$PC1)
+vulnPC2_stand<-(vuln$map$PC2)/maxValue(vuln$map$PC2)
+vulntot<-(vulnPC1_stand+vulnPC2_stand)
+vulntot_stand<-vulntot/maxValue(vulntot)
+ext <- c(-130, -20, -80, 80)
+extension <- crop(vulntot_stand, ext)
+writeRaster(extension, "vulnerability.tif")
 
-ALL THE INFORMATION PAIRS
+png("vulnerability.png", width = 6, height = 8, units = 'in', res = 3000)
+plot(extension, main="vulnerability")
+cities<- readOGR("Americas_Cities.shp")
+plot(cities, pch = 16,cex = 0.3,add=T)
+text(cities, labels=cities$NAME, cex= 0.2)
+states<- readOGR("Americas.shp")
+plot(states, lwd = 0.4, add=T)
+dev.off()
 
